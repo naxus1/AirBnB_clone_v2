e507d8de05fb216c9771a3a6b1861b369532474b
miguelfajardoc <775@holbertonschool.com>
2019-08-13 18:57:39 +0000

    [add] setup_mysql_dev

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,5 @@
+-- Prepare the mysql server for the proyect!
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON  hbnb_dev_db. * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema. * TO 'hbnb_dev'@'localhost';
