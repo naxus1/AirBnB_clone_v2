784569e0207c676bec22e3fa331018a4582ff51a
naxus1 <naxus1@gmail.com>
2019-08-15 14:49:03 +0000

    Changeto True first and last_name

--- models/user.py
+++ models/user.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+
+from models import BaseModel, Base
+from sqlalchemy import Column, String
 
 
 class User(BaseModel, Base):
@@ -13,8 +14,8 @@ class User(BaseModel, Base):
         last_name: last name
     """
 
-    __tablename__ = 'users'
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
