119e4a24afc1134e268d12424d18d207c4a0fbc3
naxus1 <naxus1@gmail.com>
2019-08-15 14:59:28 +0000

    Change Import

--- models/user.py
+++ models/user.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """This is the user class"""
 
-from models import BaseModel, Base
-from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
 
 
 class User(BaseModel, Base):
@@ -17,5 +17,5 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
