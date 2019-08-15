5978c23eacdca2ad57d0f45ae2c6fe000c42cb87
naxus1 <naxus1@gmail.com>
2019-08-15 23:24:30 +0000

    Modify 1 to 1

--- console.py
+++ console.py
@@ -55,7 +55,7 @@ class HBNBCommand(cmd.Cmd):
                     value = float(value)
                 else:
                     value = value.replace('_', ' ')
-                    value = value[1:-1]
+                   # value = value[1:-1]
                 setattr(obj, key, value)
 
             obj.save()
