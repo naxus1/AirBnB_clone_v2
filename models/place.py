60b079b51d1b955c06811f47c10be26ab9ade8e5
naxus1 <naxus1@gmail.com>
2019-08-15 17:08:04 +0000

    Add base

--- models/place.py
+++ models/place.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """This is the place class"""
-from models.base_model import BaseModel
-from sqlalchemy import Column, Integer, String, Float
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """This is the class for Place
     Attributes:
         city_id: city id
