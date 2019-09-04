a112e8820bf2a1a31b869a155d3fe790d25a191c
naxus1 <naxus1@gmail.com>
2019-09-04 18:20:10 +0000

    Fix add blank line to function

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -86,6 +86,7 @@ class DBStorage():
         Base.metadata.create_all(self.__engine)
         self.__session = scoped_session(sessionmaker(bind=self.__engine,
                                                      expire_on_commit=False))()
+
     def close(self):
         '''
              call remov
