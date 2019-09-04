2a5b0193003d1c39c6a61dbb78e87a470e44b9ce
naxus1 <naxus1@gmail.com>
2019-09-04 18:47:43 +0000

    Create point 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states_list')
+def states_list():
+    """
+    display states in html
+    Returns:
+        str: html with list states
+    """
+    return render_template("7-states_list.html", states=storage.all(State))
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """
+    display
+    Args:
+        exc.
+    """
+    storage.close()
+
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
