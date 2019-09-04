cbf4fcc1045c3547412f8db5aafe4708fe48e5d6
naxus1 <naxus1@gmail.com>
2019-09-04 20:05:26 +0000

    Fix pep8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,23 +6,22 @@ from models.state import State
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/states_list')
 def states_list():
-    """display a HTML page with a list of states
-    Returns:
-        str: rendered html
+    """
+    display comment
     """
     return render_template("7-states_list.html", states=storage.all(State))
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """called after each request
-    Args:
-        exc (Exception): unknown, seems necessary based on docu
+def teardown(error):
+    """
+    display comment
     """
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(port=5000, host="0.0.0.0")
+    app.run(host="0.0.0.0")
