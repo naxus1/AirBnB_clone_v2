6bd97f936c310aeb10f49e2317d2a3461f710090
naxus1 <naxus1@gmail.com>
2019-09-04 19:45:42 +0000

    Fix pep and list_states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
-"""
-"""
-
-from flask import Flask, render_template
+from flask import Flask, abort, render_template
 from models import storage
-from models.state import State
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/states_list')
-def states_list():
+def list_state():
     """
-    display states in html
-    Returns:
-        str: html with list states
+    Comment
     """
-    return render_template("7-states_list.html", states=storage.all(State))
+    states = []
+    for key, state in storage.all("State").items():
+        states.append({'id': state.id, 'name': state.name})
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown(exc):
+def teardown_storage(self):
     """
-    display
-    Args:
-        exc.
+    Comment
     """
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(port=5000, host="0.0.0.0")
+    app.run(host='0.0.0.0')
