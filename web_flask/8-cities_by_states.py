ea6b9aa7a0febd00b8c5dba1b18c3a3753f9cda8
naxus1 <naxus1@gmail.com>
2019-09-04 21:02:11 +0000

    Fix funtion

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -2,14 +2,18 @@
 from flask import Flask
 from flask import abort, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 @app.route('/cities_by_states')
 def cities_by_state():
-    list_state = list(storage.all("State").values())
-    return render_template('8-cities_by_states.html', list_state=list_state)
+    """
+    """
+    states=list(storage.all(State).values())
+    return render_template('8-cities_by_states.html', states=states)
+
 
 @app.teardown_appcontext
 def teardown_storage(exec):
