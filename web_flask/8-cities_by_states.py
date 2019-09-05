a11c63c3009a0ca0e893427a894c055927e71ca9
naxus1 <naxus1@gmail.com>
2019-09-05 00:17:08 +0000

    Fix variable and pep8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -7,9 +7,11 @@ from models.state import State
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/cities_by_states')
 def cities_by_state():
     """
+    display comment
     """
     states=list(storage.all(State).values())
     return render_template('8-cities_by_states.html', states=states)
@@ -17,10 +19,12 @@ def cities_by_state():
 
 @app.teardown_appcontext
 def teardown_storage(exec):
-    ''' Teardown storage
-    '''
+    """
+    display comment
+    """
+
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(port=5000, host='0.0.0.0')
